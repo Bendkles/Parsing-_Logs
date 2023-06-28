4e7a1e29cc71b35e6caf59a011f3a2f7eafe4d91
Shlomi <bendkles@duck.com>
2023-06-28 22:26:01 +0300

    updates

--- v2-parsing-app.py
+++ v2-parsing-app.py
@@ -1,94 +1,77 @@
-from flask import Flask, request, render_template, send_file
-import pandas as pd
+from flask import Flask, render_template, request, redirect, send_from_directory
+import os
+import csv
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = '/mnt/d/Cyber-HUB/LAB/Scripts Tools'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = os.path.join(app.root_path, 'uploads')
+DOWNLOAD_FOLDER = os.path.join(app.root_path, 'downloads')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
 
 def parse_transaction_log(log_file):
-    """
-    Parses the transaction log file and returns the parsed logs as a list of dictionaries.
-
-    Args:
-        log_file (str): Path to the log file.
-
-    Returns:
-        list: Parsed logs as a list of dictionaries.
-    """
-    parsed_logs = []
     with open(log_file, 'r') as file:
-        for line in file:
-            data = line.strip().split(' - ')
-            if len(data) == 6:
-                log_dict = {
-                    'TransactionID': data[0],
-                    'Timestamp': data[1],
-                    'TransactionType': data[2],
-                    'Status': data[3],
-                    'UserID': data[4],
-                    'Details': data[5]
-                }
-                parsed_logs.append(log_dict)
+        logs = file.readlines()
+
+    parsed_logs = []
+    for log in logs:
+        log_dict = {}
+        parts = log.strip().split(' - ')
+        if len(parts) == 6:
+            log_dict['TransactionID'] = parts[0]
+            log_dict['Timestamp'] = parts[1]
+            log_dict['TransactionType'] = parts[2]
+            log_dict['Status'] = parts[3]
+            log_dict['UserID'] = parts[4]
+            log_dict['Details'] = parts[5]
+            parsed_logs.append(log_dict)
+
     return parsed_logs
 
 
-@app.route('/')
+def write_to_csv(parsed_logs, output_file):
+    with open(output_file, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=parsed_logs[0].keys())
+        writer.writeheader()
+        writer.writerows(parsed_logs)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    """
-    Renders the index.html template, serving as the main menu for the web GUI.
-    """
-    return render_template('index.html')
-
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    """
-    Handles the file upload functionality.
-
-    Returns:
-        str: Response message.
-    """
-    if 'file' not in request.files:
-        return 'No file part in the request.'
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return 'No selected file.'
-
-    file.save(f"{app.config['UPLOAD_FOLDER']}/{file.filename}")
-
-    return 'File uploaded successfully.'
-
-
-@app.route('/parse')
-def parse_logs():
-    """
-    Parses the uploaded log file and saves the parsed logs as a CSV file.
-
-    Returns:
-        str: Response message.
-    """
-    log_file = f"{app.config['UPLOAD_FOLDER']}/log-file-example.txt"
-    parsed_logs = parse_transaction_log(log_file)
-    df = pd.DataFrame(parsed_logs)
-    csv_file = f"{app.config['UPLOAD_FOLDER']}/parsed_logs.csv"
-    df.to_csv(csv_file, index=False)
-    return 'Log file parsed and CSV file saved.'
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return render_template('index.html', error="No file selected.")
+        
+        file = request.files['file']
+        if file.filename == '':
+            return render_template('index.html', error="No file selected.")
+        
+        if file.filename.endswith('.txt'):
+            file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+            file.save(file_path)
+            parsed_logs = parse_transaction_log(file_path)
+            
+            if len(parsed_logs) > 0:
+                output_file = os.path.join(DOWNLOAD_FOLDER, 'parsed_logs.csv')
+                write_to_csv(parsed_logs, output_file)
+                return redirect('/download')
+            else:
+                return render_template('index.html', error="Failed to parse the file.")
+        else:
+            return render_template('index.html', error="Invalid file format. Only .txt files are supported.")
+    else:
+        return render_template('index.html', error="")
 
 
 @app.route('/download')
-def download_file():
-    """
-    Allows the user to download the parsed CSV file.
-
-    Returns:
-        file: The parsed CSV file.
-    """
-    csv_file = f"{app.config['UPLOAD_FOLDER']}/parsed_logs.csv"
-    return send_file(csv_file, as_attachment=True)
+def download():
+    file_name = 'parsed_logs.csv'
+    file_path = os.path.join(DOWNLOAD_FOLDER, file_name)
+    
+    if os.path.exists(file_path):
+        return send_from_directory(DOWNLOAD_FOLDER, file_name, as_attachment=True)
+    else:
+        return render_template('index.html', error="Failed to download the file.")
 
 
 if __name__ == '__main__':
